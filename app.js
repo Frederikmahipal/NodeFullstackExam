310f634da0e9a61c46908e02bcbcbc133a710ad9
Frederik Vilhelmsen <frederikvilhelmsen@Frederiks-MacBook-Pro.local>
2022-12-18 20:03:18 +0100

    updating database

--- app.js
+++ app.js
@@ -3,25 +3,16 @@ const app = express()
 const port = 8080
 const mysql = require('mysql')
 const dotenv = require('dotenv')
+const authRouter = require('./routes/authRouter')
 
 dotenv.config()
 
-var db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "sildsild1"
-  });
 
-  db.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  })
 
-  
 
-app.get('/', (req, res) => {
-  res.send('ayo')
-})
+app.use(authRouter)
+
 
 app.listen(port)
 console.log('app: ' + port)
+
