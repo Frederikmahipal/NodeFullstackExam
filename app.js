b5fe0b758163968d223e783c17a26664addf2156
Frederik Vilhelmsen <frederikvilhelmsen@Frederiks-MacBook-Pro.local>
2023-01-04 13:28:26 +0100

    setup db

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express')
-const app = express()
-const port = 8080
-const mysql = require('mysql')
-const dotenv = require('dotenv')
-const authRouter = require('./routes/authRouter')
+const app = express();
+const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-dotenv.config()
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}, (error) => {
+    if (error) {
+        throw error
+    }
+    console.log('connected')
+})
 
+//middleware
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
+//routes
+app.use('/', require('./routes/index'))
+app.use('/users', require('./routes/users'))
 
+const port = process.env.port || 8080;
 
-app.use(authRouter)
-
-
-app.listen(port)
-console.log('app: ' + port)
-
+app.listen(port, console.log(`Server on port ${port}`))
